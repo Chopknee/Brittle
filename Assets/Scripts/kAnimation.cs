1bbb38df75603978a9489d9480dd56a6723c7470
BuildTools <chopknee@gmail.com>
2019-02-09 14:33:01 -0500

    Added existing project files.

---
+++ Assets/Scripts/kAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kAnimation : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis ("Vertical");
+		anim.SetFloat ("Speed", move);
+
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			anim.SetTrigger ("isJumping");
+		}
+
+	}
+}
---
+++ Assets/Scripts/onMouseDrag.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onMouseDrag : MonoBehaviour {
+
+	Vector3 distance;
+
+	void OnMouseDown()
+	{
+		distance = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,Camera.main.WorldToScreenPoint(transform.position).z)) - transform.position;
+	}
+
+
+	void OnMouseDrag ()
+	{
+		Vector3 distance_to_screen = Camera.main.WorldToScreenPoint(transform.position);
+		Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen.z ));
+		transform.position = new Vector3( pos_move.x - distance.x , pos_move.y -distance.y , pos_move.z -distance.z );
+
+	}
+}
