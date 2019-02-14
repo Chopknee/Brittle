80d6b0fd6abde05c64ba3a3993d0c9bfc951cb86
Branwink <branwink@iu.edu>
2019-02-14 12:58:52 -0500

    This commit includes a very short prototype level and hive eneymy.

---
+++ Assets/Scripts/HiveEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiveEnemy : MonoBehaviour {
+    public Transform target;
+    public Transform hive;
+    public float chaseRange;
+    public float hiveRange;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+        float distanceToHive = Vector3.Distance(transform.position, hive.position);
+        if(distanceToTarget < chaseRange && distanceToHive < hiveRange)
+        {
+
+            Vector3 targetDir = target.position - transform.position;
+            float angleOne = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg -90f;
+            Quaternion r = Quaternion.AngleAxis(angleOne, Vector3.forward);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, r, 360);
+
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+
+        else
+        {
+            Vector3 hiveLocationDir = hive.position - transform.position;
+            float angle = Mathf.Atan2(hiveLocationDir.y, hiveLocationDir.x) * Mathf.Rad2Deg - 90f;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 360);
+
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+    }
+}
