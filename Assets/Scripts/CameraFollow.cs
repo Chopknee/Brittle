35089b93b5a4fc857da4d870e1cfee23e5581eb6
BuildTools <chopknee@gmail.com>
2019-02-15 01:43:03 -0500

    Added physics based object dragging. Adjusted camera follow script. Fleshed out level 1 demo.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,11 +7,13 @@ public class CameraFollow : MonoBehaviour {
     public Vector2 minPosition = new Vector2(-10, -10);
     public Vector2 maxPosition = new Vector2(10, 10);
     public GameObject followTarget;
-    [Range(0.01f, 1)]
-    public float followSpeed = 0.95f;
     public bool drawBoundsGizmo = true;
-	// Use this for initialization
-	void Start () {
+    [Tooltip("Controls how the camera settles on the target object when it stops moving.")]
+    public AnimationCurve followCurve;
+    [Range(0.5f, 4), Tooltip("Controls how far from the center of the camera the target can get.")]
+    public float multiplier = 1;
+    // Use this for initialization
+    void Start () {
         if (followTarget == null) {
             followTarget = GameObject.FindGameObjectWithTag("Player");
         }
@@ -20,24 +22,29 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (followTarget != null) {
-            //Follow the target, keeping the camera within the defined bounds
-
-            //get distance between camera and target
+            
+            //get the difference between the camera and player
             Vector3 diff = followTarget.transform.position - transform.position;
 
-            //set the diff y to 0 because we don't want to change the current height
+            //The z should be forced to 0 because we are on a 2d plane
             diff.z = 0;
-            //tween that difference
-            diff = diff * followSpeed;//Move 10 % of the distance toward the target
-            diff = diff * Time.deltaTime;//Make it time dependent
-            diff = transform.position + diff;//Predict the new postion
+            //Take the magnitude (distance from target) and scale it down. We only want the camera to move a portion of the distance from the target.
+            //This number grows larger as the distance gets bigger, until the mul is equal to the distance moved by the target since the last frame.
+            //Time.deltaTime ensures that this is the same on any framerate
+            float mul = followCurve.Evaluate(diff.magnitude) * multiplier * Time.deltaTime;
+            
+            //This is now applied to the position
+            diff = transform.position + (diff * mul);
+            //Make sure it is within the current bounds.
             diff.x = Mathf.Max(Mathf.Min(diff.x, maxPosition.x), minPosition.x);
             diff.y = Mathf.Max(Mathf.Min(diff.y, maxPosition.y), minPosition.y);
-            transform.position = diff;//Finally set the position of the camera!
+
+            transform.position = diff;//Finally we can set the position of the camera!
         }
 	}
 
     private void OnDrawGizmos() {
+        //Visualizes the boundaries of where the camera is able to travel.
         if (drawBoundsGizmo) {
             Gizmos.color = new Color(0, 0, 255);
             //Upper line
--- Assets/Scripts/FireflyDraggable.cs
+++ Assets/Scripts/FireflyDraggable.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireflyDraggable : MonoBehaviour {
+    [Tooltip("The script automatically looks for an object tagged as main camera. Only set this if you need to use a custom camera.")]
+    public Camera mainCamera;
+    [Tooltip("True when the object is being dragged.")]
+    public bool dragging = false;
+    [Tooltip("How much force to apply to the object when being dragged.")]
+    public float multiplier = 0.25f;
+    public float maxAltitude = 10f;
+    [Tooltip("Sets how the object will behave as it reaches its maximum altitude.")]
+    public AnimationCurve altitudeApproachCurve;
+
+    private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        rb = GetComponent<Rigidbody2D>();
+        if (mainCamera == null) {
+            mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (dragging) {
+            //Get the position of the mouse in world space
+            Vector2 mouseWorldPos = mainCamera.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            //Create a normalized vector (we aren't worried about the distance from the mouse cursor, only the direction) pointing in the direction of the cursor
+            Vector2 directionVector = (mouseWorldPos - (Vector2)transform.position).normalized;
+            //Apply it as a force and multiply it to get a visible result
+            rb.AddForce(directionVector * multiplier * altitudeApproachCurve.Evaluate(maxAltitude - transform.position.y));
+        }
 	}
+
+    private void OnMouseDown() {
+        dragging = true;
+    }
+
+    private void OnMouseUp() {
+        dragging = false;
+    }
 }
