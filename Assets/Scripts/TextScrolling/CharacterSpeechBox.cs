4be952fd3e3fd6aeecc5e9fdf79cd3711876be5e
Chopknee <Chopknee@gmail.com>
2019-04-13 01:47:49 -0400

    Created a simple dialog script system.

---
+++ Assets/Scripts/TextScrolling/CharacterSpeechBox.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSpeechBox : MonoBehaviour {
+
+    [SerializeField]
+    public ChatBox[] Dialog;
+    public Image CharacterImage;
+    public bool playOnAwake = false;
+
+    public int DialogIndex {
+        get {
+            return dialogIndex;
+        }
+        set {
+            if (value > -1 && value < Dialog.Length) {
+                dialogIndex = value;
+                SetCurrentDialog();
+                myTextScrollInteractive.BeginScroll(Dialog[dialogIndex].Speech);
+            } else {
+                throw new Exception("Invalid range!");
+            }
+        }
+    }
+
+    private int dialogIndex = 0;
+
+    private bool canPlay = false;
+
+    public bool Playing {
+        get {
+            return playing;
+        }
+        set {
+            playing = value;
+            if (playing) {
+                BeginDialog();
+            } else {
+                myTextScrollInteractive.EndScroll();
+            }
+        }
+    }
+
+    private TextScrollInteractive myTextScrollInteractive;
+
+    private bool playing = false;
+	// Use this for initialization
+	void Start () {
+		if (Dialog != null && Dialog.Length > 0 && GetComponentInChildren<TextScrollInteractive>() != null) {
+            myTextScrollInteractive = GetComponentInChildren<TextScrollInteractive>();
+            myTextScrollInteractive.OnFinished += SpeechFinished;
+            canPlay = true;
+        }
+        if (playOnAwake) {
+            Playing = true;
+        }
+	}
+
+    public void SetCurrentDialog() {
+        if (canPlay) {
+            myTextScrollInteractive.SpeechText = Dialog[dialogIndex].Speech;
+            myTextScrollInteractive.CharacterDelay = Dialog[dialogIndex].CharacterDelay;
+            myTextScrollInteractive.periodDelay = Dialog[dialogIndex].periodDelay;
+            myTextScrollInteractive.commaDelay = Dialog[dialogIndex].commaDelay;
+            myTextScrollInteractive.onFinishDelay = Dialog[dialogIndex].OnFinishDelay;
+            if (CharacterImage != null) {
+                CharacterImage.sprite = Dialog[dialogIndex].Character;
+            }
+        }
+    }
+
+    public void SpeechFinished() {
+
+        dialogIndex++;
+        if (dialogIndex >= Dialog.Length || !playing) {
+            playing = false;
+            return;
+        }
+
+        SetCurrentDialog();
+        myTextScrollInteractive.BeginScroll(Dialog[dialogIndex].Speech);
+
+    }
+
+    public void BeginDialog() {
+        playing = true;
+        SetCurrentDialog();
+        myTextScrollInteractive.BeginScroll(Dialog[dialogIndex].Speech); 
+    }
+
+    [Serializable]
+    public class ChatBox {
+        public Sprite Character;
+        [TextArea]
+        public string Speech = "";
+        [Range(1, 500)]
+        public float CharacterDelay = 80;
+        [Range(1, 500)]
+        public float periodDelay = 150;
+        [Range(1, 500)]
+        public float commaDelay = 125;
+        [Range(0, 5000)]
+        public float OnFinishDelay = 100;
+    }
+}
---
+++ Assets/Scripts/TextScrolling/TextScrollInteractive.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScrollInteractive: MonoBehaviour {
+
+    [TextArea]
+    public string SpeechText;
+    [Range(0f, 1f)]
+    public float scrollVolume = .2f;
+    public AudioClip scrollSound;
+
+    [Range(1, 500)]
+    public float CharacterDelay = 80;
+    [Range(1, 500)]
+    public float periodDelay = 150;
+    [Range(1, 500)]
+    public float commaDelay = 125;
+    [Range(0, 5000)]
+    public float onFinishDelay = 100;
+
+    public delegate void Finished ();
+    public Finished OnFinished;
+
+    public Text myText;
+
+    public char[] scrollTextSplit;
+    public string currentText = "";
+
+    public float next;
+    public bool scroll;
+
+    public int i = 0;
+
+    public void Awake () {
+
+        scrollTextSplit = SpeechText.ToCharArray();
+
+        if (scrollSound != null && GetComponent<AudioSource>() != null) {
+            GetComponent<AudioSource>().clip = scrollSound;
+            GetComponent<AudioSource>().volume = scrollVolume;
+        }
+
+        if (myText == null) {
+            myText = GetComponent<Text>();
+        }
+
+        currentText = "";
+    }
+
+    public void Update () {
+
+        if (scroll && Time.time > next) {
+
+            next = Time.time + (CharacterDelay / 1000);
+            Debug.Log("I AM ACTUALLY RUNNING");
+            if (i < scrollTextSplit.Length) {
+                switch (scrollTextSplit[i]) {
+                    case ',':
+                        next = Time.time + (commaDelay / 1000);
+                        break;
+                    case '.':
+                        next = Time.time + (periodDelay / 1000);
+                        break;
+                    case '!':
+                        next = Time.time + (periodDelay / 1000);
+                        break;
+                    case '?':
+                        next = Time.time + (periodDelay / 1000);
+                        break;
+                }
+                currentText += scrollTextSplit[i];
+
+                if (scrollSound != null && GetComponent<AudioSource>() != null) {
+                    GetComponent<AudioSource>().Play();
+                }
+                myText.text = currentText;
+            }
+
+            i++;
+
+            if (i == scrollTextSplit.Length) {
+
+                next = Time.time + (onFinishDelay / 1000);
+
+            } else if (i > scrollTextSplit.Length) {
+                scroll = false;
+                if (OnFinished != null)
+                    OnFinished();
+            }
+        }
+
+    }
+
+    public void BeginScroll ( string speechText ) {
+        SpeechText = speechText;
+        scrollTextSplit = SpeechText.ToCharArray();
+        currentText = "";
+        myText.text = currentText;
+        i = 0;
+        next = Time.time;
+        scroll = true;
+        Debug.Log("Begin scrolling");
+    }
+
+    public void EndScroll() {
+        scroll = false;
+    }
+
+
+}
